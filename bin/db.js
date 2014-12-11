ec0551a60189e94262d22f4fca2ec9ab69303eff
Andreas Zaschka <andreas.zaschka@mailbox.tu-dresden.de>
2014-12-12 00:34:01 +0100

    Version 0.1.8 new db-script

--- bin/db.js
+++ bin/db.js
@@ -2,16 +2,15 @@
  * Created by Andreas on 11.12.2014.
  */
 var cc = require('config-multipaas'),
-    pg = require('pg-query')
+    pg = require('pg')
 
 var table_counters = 'COUNTERS',
     table_counter_detail = 'COUNTER_DETAILS',
     table_counter_history = 'COUNTER_HISTORY'
 
-var config = cc({
-    table_name: process.env.TABLE_NAME || process.env.OPENSHIFT_APP_NAME || 'counter'
-})
-var pg_config = config.get('POSTGRESQL_DB_URL');
+var conString = "pg://admin2efblhn:eupljg6eQRSH@$OPENSHIFT_POSTGRESQL_DB_HOST:$OPENSHIFT_POSTGRESQL_DB_PORT/counter";
+var client = new pg.Client(conString);
+client.connect();
 
 var error_response = "data already exists - bypassing db initialization step\n";
 
@@ -21,39 +20,57 @@ function createDBSchema(err, rows, result) {
     }
 
     // TABLE COUNTERS
-    pg.connectionParameters = pg_config + '/' + table_counters;
-    console.log(pg_config + '/' + table_counters);
-    var query = "CREATE TABLE " + table_counters + " (" +
+    var query = "CREATE TABLE IF NOT EXISTS " + table_counters + " (" +
         "id serial NOT NULL, " +
         "name character varying(14) NOT NULL, " +
         "amount integer NOT NULL," +
         "PRIMARY KEY (id)" +
         ");";
-    pg(query);
+    console.log(query);
+    var pg = client.query(query);
+    pg.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    pg.on("end", function (row, result) {
+        console.log(result.rowCount + ' rows were received');
+        client.end();
+    });
 
     //TABLE COUNTER_DETAILS
-    pg.connectionParameters = pg_config + '/' + table_counter_detail;
-    console.log(pg_config + '/' + table_counter_detail);
-    var query = "CREATE TABLE " + table_counter_detail + " (" +
+    var query = "CREATE TABLE IF NOT EXISTS " + table_counter_detail + " (" +
         "id serial NOT NULL REFERENCES " + table_counters + "(id), " +
         "name character varying(14) NOT NULL, " +
         "amount integer NOT NULL," +
         "mod_date timestamp DEFAULT CURRENT_TIMESTAMP," +
         "mod_username character varying(14) DEFAULT 'anonymous'" +
         ");";
-    pg(query);
+    console.log(query);
+    var pg = client.query(query);
+    pg.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    pg.on("end", function (row, result) {
+        console.log(result.rowCount + ' rows were received');
+        client.end();
+    });
 
     //TABLE COUNTER_HISTORY
-    pg.connectionParameters = pg_config + '/' + table_counter_history;
-    console.log(pg_config + '/' + table_counter_history);
-    var query = "CREATE TABLE " + table_counter_history + " (" +
+    var query = "CREATE TABLE IF NOT EXISTS " + table_counter_history + " (" +
         "id serial NOT NULL REFERENCES " + table_counters + "(id), " +
         "name character varying(14) NOT NULL, " +
         "amount integer NOT NULL," +
         "mod_date timestamp DEFAULT CURRENT_TIMESTAMP," +
         "mod_username character varying(14) DEFAULT 'anonymous'" +
         ");";
-    pg(query);
+    console.log(query);
+    var pg = client.query(query);
+    pg.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    pg.on("end", function (row, result) {
+        console.log(result.rowCount + ' rows were received');
+        client.end();
+    });
 };
 
 function init_db() {
@@ -79,42 +96,45 @@ function flush_db() {
 }
 
 function select_all(req, res, next) {
-    console.log(pg);
-    pg('SELECT * FROM ' + table_counters + ';', function (err, rows, result) {
-        console.log(config);
-        if (err) {
-            res.send(500, {http_status: 500, error_msg: err})
-            return console.error('error running query', err);
-        }
+    var query = 'SELECT * FROM ' + table_counters + ';';
+    console.log(query);
+    var pg = client.query(query);
+    pg.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    pg.on("end", function (row, result) {
+        console.log(result.rowCount + ' rows were received');
         res.send(result);
-        return rows;
+        client.end();
     });
 };
 
 function select_one(req, res, next) {
     var id = req.params.id;
-    console.log(pg);
-    pg('SELECT * FROM ' + table_counter_detail + ' WHERE id=' + id + ';', function (err, rows, result) {
-        console.log(config);
-        if (err) {
-            res.send(500, {http_status: 500, error_msg: err})
-            return consile.error('error running query', err);
-        }
+    var query = 'SELECT * FROM ' + table_counter_detail + ' WHERE id=' + id + ';';
+    console.log(query);
+    var pg = client.query(query);
+    pg.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    pg.on("end", function (row, result) {
+        console.log(result.rowCount + ' rows were received');
         res.send(result);
-        return rows;
+        client.end();
     });
 }
 
 function insert_dummy(req, res, next) {
-    console.log(pg);
-    pg('INSERT INTO ' + table_counters + ' (name, amount) VALUES ("MATE", 37),("IPhone", 1));', function (err, rows, result) {
-        console.log(config);
-        if (err) {
-            res.send(500, {http_status: 500, error_msg: err})
-            return consile.error('error running query', err);
-        }
+    var query = 'INSERT INTO ' + table_counters + ' (name, amount) VALUES ("MATE", 37),("IPhone", 1));';
+    console.log(query);
+    var pg = client.query(query);
+    pg.on("row", function (row, result) {
+        result.addRow(row);
+    });
+    pg.on("end", function (row, result) {
+        console.log(result.rowCount + ' rows were received');
         res.send(result);
-        return rows;
+        client.end();
     });
 }
 
