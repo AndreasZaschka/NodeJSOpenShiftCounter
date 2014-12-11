d1af6c4040ee491e09498e1f0c43ac4cef688947
Andreas Zaschka <myemailfach@gmail.com>
2014-12-11 11:22:16 +0100

    Version 0.1.0

---
+++ bin/db.js
@@ -0,0 +1,98 @@
+/**
+ * Created by Andreas on 11.12.2014.
+ */
+var cc = require('config-multipaas'),
+    pg = require('pg-query')
+
+var table_counters = 'COUNTERS',
+    table_counter_detail = 'COUNTER_DETAILS',
+    table_counter_history = 'COUNTER_HISTORY'
+
+var config = cc({
+    table_name: process.env.TABLE_NAME || process.env.OPENSHIFT_APP_NAME || 'COUNTERS'
+})
+var pg_config = config.get('POSTGRESQL_DB_URL');
+
+var error_response = "data already exists - bypassing db initialization step\n";
+
+function createDBSchema(err, rows, result) {
+    if (err && err.code == "ECONNREFUSED") {
+        return console.error("DB connection unavailable, see README notes for setup assistance\n", err);
+    }
+
+    // TABLE COUNTERS
+    var query = "CREATE TABLE " + table_counters + " (" +
+        "id serial NOT NULL, " +
+        "name character varying(14) NOT NULL, " +
+        "amount integer NOT NULL," +
+        "PRIMARY KEY (id)" +
+        ");";
+    pg.connectionParameters = pg_config + '/' + table_counters;
+    console.log(pg_config + '/' + table_counters);
+    pg(query);
+
+    //TABLE COUNTER_DETAILS
+    var query = "CREATE TABLE " + table_counter_detail + " (" +
+        "id serial NOT NULL REFERENCES " + table_counters + "(id), " +
+        "name character varying(14) NOT NULL, " +
+        "amount integer NOT NULL," +
+        "mod_date timestamp DEFAULT CURRENT_TIMESTAMP," +
+        "mod_username character varying(14) DEFAULT 'anonymous'" +
+        ");";
+    pg.connectionParameters = pg_config + '/' + table_counter_detail;
+    console.log(pg_config + '/' + table_counter_detail);
+    pg(query);
+
+    //TABLE COUNTER_HISTORY
+    var query = "CREATE TABLE " + table_counter_history + " (" +
+        "id serial NOT NULL REFERENCES " + table_counters + "(id), " +
+        "name character varying(14) NOT NULL, " +
+        "amount integer NOT NULL," +
+        "mod_date timestamp DEFAULT CURRENT_TIMESTAMP," +
+        "mod_username character varying(14) DEFAULT 'anonymous'" +
+        ");";
+    pg.connectionParameters = pg_config + '/' + table_counter_history;
+    console.log(pg_config + '/' + table_counter_history);
+    pg(query);
+};
+
+function init_db() {
+    pg('CREATE EXTENSION postgis;', createDBSchema);
+}
+
+function flush_db() {
+    pg('DROP TABLE ' + table_counters + ';', function (err, rows, result) {
+        var response = 'Database ' + table_counters + ' dropped!';
+        console.log(response);
+        return response;
+    });
+    pg('DROP TABLE ' + table_counter_detail + ';', function (err, rows, result) {
+        var response = 'Database ' + table_counter_detail + ' dropped!';
+        console.log(response);
+        return response;
+    });
+    pg('DROP TABLE ' + table_counter_history + ';', function (err, rows, result) {
+        var response = 'Database ' + table_counter_history + ' dropped!';
+        console.log(response);
+        return response;
+    });
+}
+
+function select_all(req, res, next) {
+    console.log(pg);
+    pg('SELECT id,name,amount FROM ' + table_counters + ';', function (err, rows, result) {
+        console.log(config);
+        if (err) {
+            res.send(500, {http_status: 500, error_msg: err})
+            return console.error('error running query', err);
+        }
+        res.send(result);
+        return rows;
+    });
+};
+
+module.exports = exports = {
+    selectAll: select_all,
+    flushDB: flush_db,
+    initDB: init_db
+};
